856f92b4dfe4c21094fdaf3cd66d0ffc47d61e55
Mikka <telfax@yandex.ru>
2022-09-04 20:20:47 +0300

    add some interface example

---
+++ GolangMK/interface/example/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Article struct {
+	Title  string
+	Author string
+}
+
+func (a Article) String() string {
+	return fmt.Sprintf("The %s article written by %s", a.Title, a.Author)
+}
+
+func Print(s fmt.Stringer) {
+	fmt.Println(s)
+}
+
+func main() {
+	art := Article{
+		Title:  "Success",
+		Author: "Mike",
+	}
+
+	Print(art)
+}
